879435b131b7d516cde3a9144ec79ec206f00792
Jean Viegas <jeanmelg@gmail.com>
2022-09-28 19:02:28 -0300

    feat(scraper): :sparkles: scrape news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,30 @@
+import time
+
+import requests
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    time.sleep(1)
+    headers = {"user-agent": "Fake user-agent"}
+    try:
+        response = requests.get(url, timeout=3, headers=headers)
+        if response.status_code != 200:
+            return None
+    except (requests.HTTPError, requests.ReadTimeout):
+        return None
+    else:
+        return response.text
 
 
 # Requisito 2
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    posts_links = selector.css('.entry-title a::attr(href)').getall()
+    return posts_links
 
 
 # Requisito 3
