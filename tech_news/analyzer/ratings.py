06d0130128de918b5f787853da6193f15a7f9bf8
Jean Viegas <jeanmelg@gmail.com>
2022-09-30 14:01:51 -0300

    feat(ratings): :sparkles: top 5 news function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,15 @@
+from tech_news.database import search_news
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    news = search_news({})
+    news.sort(key=lambda x: x["comments_count"], reverse=True)
+
+    if len(news) < 5:
+        return [(item["title"], item["url"]) for item in news]
+    return [(item["title"], item["url"]) for item in news][:5]
 
 
 # Requisito 11
