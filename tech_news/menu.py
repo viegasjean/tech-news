ca3f5ef99948536778594c62ab4e2e6800f10bb8
Jean Viegas <jeanmelg@gmail.com>
2022-10-04 20:03:10 -0300

    change if elif to match case

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -2,9 +2,12 @@
 import sys
 
 from tech_news.analyzer.ratings import top_5_categories, top_5_news
-from tech_news.analyzer.search_engine import (search_by_category,
-                                              search_by_date, search_by_tag,
-                                              search_by_title)
+from tech_news.analyzer.search_engine import (
+    search_by_category,
+    search_by_date,
+    search_by_tag,
+    search_by_title,
+)
 from tech_news.scraper import get_tech_news
 
 
@@ -17,29 +20,54 @@ def analyzer_menu():
         "5 - Listar top 5 notícias;\n 6 - Listar top 5 categorias;\n 7 - Sair."
     )
 
-    if option == "0":
-        amount = input("Digite quantas notícias serão buscadas:")
-        get_tech_news(amount)
-    elif option == "1":
-        title = input("Digite o título:")
-        search_by_title(title)
-    elif option == "2":
-        date = input("Digite a data no formato aaaa-mm-dd:")
-        search_by_date(date)
-    elif option == "3":
-        tag = input("Digite a tag:")
-        search_by_tag(tag)
-    elif option == "4":
-        category = input("Digite a categoria:")
-        search_by_category(category)
-    elif option == "5":
-        top_5_news()
-    elif option == "6":
-        top_5_categories()
-    elif option == "7":
-        print('Encerrando script')
-    else:
-        print("Opção inválida", file=sys.stderr)
+    match option:
+        case "0":
+            amount = input("Digite quantas notícias serão buscadas:")
+            get_tech_news(amount)
+        case "1":
+            title = input("Digite o título:")
+            search_by_title(title)
+        case "2":
+            date = input("Digite a data no formato aaaa-mm-dd:")
+            search_by_date(date)
+        case "3":
+            tag = input("Digite a tag:")
+            search_by_tag(tag)
+        case "4":
+            category = input("Digite a categoria:")
+            search_by_category(category)
+        case "5":
+            top_5_news()
+        case "6":
+            top_5_categories()
+        case "7":
+            print("Encerrando script")
+        case _:
+            print("Opção inválida", file=sys.stderr)
+
+    # if option == "0":
+    #     amount = input("Digite quantas notícias serão buscadas:")
+    #     get_tech_news(amount)
+    # case "1":
+    #     title = input("Digite o título:")
+    #     search_by_title(title)
+    # case "2":
+    #     date = input("Digite a data no formato aaaa-mm-dd:")
+    #     search_by_date(date)
+    # case "3":
+    #     tag = input("Digite a tag:")
+    #     search_by_tag(tag)
+    # case "4":
+    #     category = input("Digite a categoria:")
+    #     search_by_category(category)
+    # case "5":
+    #     top_5_news()
+    # case "6":
+    #     top_5_categories()
+    # case "7":
+    #     print('Encerrando script')
+    # else:
+    #     print("Opção inválida", file=sys.stderr)
 
 
 if __name__ == "__main__":
