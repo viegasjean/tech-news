a553c73702a0a5b6391a250d188bd4b0fb47eed5
Jean Viegas <jeanmelg@gmail.com>
2022-10-04 21:48:51 -0300

    refactor: :recycle: use dictionaries strategie to turn function less complex

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -2,13 +2,43 @@
 import sys
 
 from tech_news.analyzer.ratings import top_5_categories, top_5_news
-from tech_news.analyzer.search_engine import (
-    search_by_category,
-    search_by_date,
-    search_by_tag,
-    search_by_title,
-)
+from tech_news.analyzer.search_engine import (search_by_category,
+                                              search_by_date, search_by_tag,
+                                              search_by_title)
 from tech_news.scraper import get_tech_news
+
+
+def news_amount():
+    amount = input("Digite quantas notícias serão buscadas:")
+    get_tech_news(amount)
+
+
+def by_title():
+    title = input("Digite o título:")
+    search_by_title(title)
+
+
+def by_date():
+    date = input("Digite a data no formato aaaa-mm-dd:")
+    search_by_date(date)
+
+
+def by_tag():
+    tag = input("Digite a tag:")
+    search_by_tag(tag)
+
+
+def by_category():
+    category = input("Digite a categoria:")
+    search_by_category(category)
+
+
+def finish():
+    print("Encerrando script")
+
+
+def invalid_option():
+    print("Opção inválida", file=sys.stderr)
 
 
 def analyzer_menu():
@@ -20,54 +50,18 @@ def analyzer_menu():
         "5 - Listar top 5 notícias;\n 6 - Listar top 5 categorias;\n 7 - Sair."
     )
 
-    match option:
-        case "0":
-            amount = input("Digite quantas notícias serão buscadas:")
-            get_tech_news(amount)
-        case "1":
-            title = input("Digite o título:")
-            search_by_title(title)
-        case "2":
-            date = input("Digite a data no formato aaaa-mm-dd:")
-            search_by_date(date)
-        case "3":
-            tag = input("Digite a tag:")
-            search_by_tag(tag)
-        case "4":
-            category = input("Digite a categoria:")
-            search_by_category(category)
-        case "5":
-            top_5_news()
-        case "6":
-            top_5_categories()
-        case "7":
-            print("Encerrando script")
-        case _:
-            print("Opção inválida", file=sys.stderr)
+    options = {
+        "0": news_amount,
+        "1": by_title,
+        "2": by_date,
+        "3": by_tag,
+        "4": by_category,
+        "5": top_5_news,
+        "6": top_5_categories,
+        "7": finish,
+    }
 
-    # if option == "0":
-    #     amount = input("Digite quantas notícias serão buscadas:")
-    #     get_tech_news(amount)
-    # case "1":
-    #     title = input("Digite o título:")
-    #     search_by_title(title)
-    # case "2":
-    #     date = input("Digite a data no formato aaaa-mm-dd:")
-    #     search_by_date(date)
-    # case "3":
-    #     tag = input("Digite a tag:")
-    #     search_by_tag(tag)
-    # case "4":
-    #     category = input("Digite a categoria:")
-    #     search_by_category(category)
-    # case "5":
-    #     top_5_news()
-    # case "6":
-    #     top_5_categories()
-    # case "7":
-    #     print('Encerrando script')
-    # else:
-    #     print("Opção inválida", file=sys.stderr)
+    options.get(option, invalid_option)()
 
 
 if __name__ == "__main__":
